1e09c10dbf3abbf27f76ba272494a1b48115671f
Sérgio Gomes <mail@sgomes.com>
2019-01-25 17:42:56 +0000

    Unify browserslist usage based on package.json.
    This commit:
    - Removes duplicate browserslist definition in Babel config.
    - Uses the browserslist definition to configure Terser ES6 and
      browser-specific settings.
    - Prepares package.json for multiple browserslists, to enable
      builds for multiple targets.
    - Defines the BROWSERSLIST_ENV environment variable in
      `webpack.config.js`, which is used by all browserslist-aware
      tools to pick the right configuration. This will enable builds
      for multiple targets to be controlled by WebPack.

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,8 @@ const CircularDependencyPlugin = require( 'circular-dependency-plugin' );
 const DuplicatePackageCheckerPlugin = require( 'duplicate-package-checker-webpack-plugin' );
 const MomentTimezoneDataPlugin = require( 'moment-timezone-data-webpack-plugin' );
 const FilterWarningsPlugin = require( 'webpack-filter-warnings-plugin' );
+const browserslist = require( 'browserslist' );
+const caniuse = require( 'caniuse-api' );
 
 /**
  * Internal dependencies
@@ -43,6 +45,10 @@ const shouldEmitStatsWithReasons = process.env.EMIT_STATS === 'withreasons';
 const shouldCheckForCycles = process.env.CHECK_CYCLES === 'true';
 const codeSplit = config.isEnabled( 'code-splitting' );
 const isCalypsoClient = process.env.CALYPSO_CLIENT === 'true';
+
+const browserslistEnvironment = 'defaults';
+const browsers = browserslist( null, { env: browserslistEnvironment } );
+process.env.BROWSERSLIST_ENV = browserslistEnvironment;
 
 /**
  * Plugin that generates the `public/custom-properties.css` file before compilation
@@ -157,6 +163,17 @@ const wordpressExternals = ( context, request, callback ) =>
 		? callback( null, `root ${ wordpressRequire( request ) }` )
 		: callback();
 
+function chooseTerserEcmaVersion( supportedBrowsers ) {
+	if ( ! caniuse.isSupported( 'arrow-functions', supportedBrowsers ) ) {
+		return 5;
+	}
+	if ( ! caniuse.isSupported( 'es6-class', supportedBrowsers ) ) {
+		return 5;
+	}
+
+	return 6;
+}
+
 /**
  * Return a webpack config object
  *
@@ -208,8 +225,11 @@ function getWebpackConfig( {
 					parallel: workerCount,
 					sourceMap: Boolean( process.env.SOURCEMAP ),
 					terserOptions: {
-						ecma: 5,
-						safari10: true,
+						ecma: chooseTerserEcmaVersion( browsers ),
+						ie8: browsers.includes( 'ie 8' ),
+						safari10: browsers.some(
+							browser => browser.includes( 'safari 10' ) || browser.includes( 'ios_saf 10' )
+						),
 					},
 				} ),
 			],
