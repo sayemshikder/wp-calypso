3a2faac0b0265df00e42ddcf69d5d89c714b5d66
Caleb Bauermeister <cbauerman@gmail.com>
2019-02-21 14:30:43 -0800

    add snapshot tests for gsuite purchase cta features

---
+++ client/my-sites/domains/domain-management/gsuite/gsuite-purchase-cta/test/features.js
@@ -0,0 +1,42 @@
+/** @format */
+/**
+ * External dependencies
+ */
+import { Provider } from 'react-redux';
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+/**
+ * Internal dependencies
+ */
+import { createReduxStore } from 'state';
+import GSuitePurchaseCtaFeatures from '../features';
+
+describe( 'GSuitePurchaseCtaFeatures', () => {
+	test( 'it renders GSuitePurchaseCtaFeatures with basic plan', () => {
+		const store = createReduxStore();
+		const tree = renderer
+			.create(
+				<Provider store={ store }>
+					<GSuitePurchaseCtaFeatures domainName={ 'testing123.com' } productSlug={ 'gapps' } />
+				</Provider>
+			)
+			.toJSON();
+		expect( tree ).toMatchSnapshot();
+	} );
+
+	test( 'it renders GSuitePurchaseCtaFeatures with business plan', () => {
+		const store = createReduxStore();
+		const tree = renderer
+			.create(
+				<Provider store={ store }>
+					<GSuitePurchaseCtaFeatures
+						domainName={ 'testing123.com' }
+						productSlug={ 'gappsbusiness' }
+					/>
+				</Provider>
+			)
+			.toJSON();
+		expect( tree ).toMatchSnapshot();
+	} );
+} );
