f2bc3fc5031737a56bbe97c9dd0155e7fbed14f3
Thomas Guillot <thomasguillot@users.noreply.github.com>
2019-02-22 12:22:21 +0000

    Tiled Gallery: Fix uppercase for filter title

--- client/gutenberg/extensions/tiled-gallery/filter-toolbar.js
+++ client/gutenberg/extensions/tiled-gallery/filter-toolbar.js
@@ -28,7 +28,7 @@ const availableFilters = [
 				<Path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16l-7-8v8H5l7-8V5h7v14z" />
 			</SVG>
 		),
-		title: _x( 'Black and white', 'image style' ),
+		title: _x( 'Black and White', 'image style' ),
 		value: 'black-and-white',
 	},
 	{
